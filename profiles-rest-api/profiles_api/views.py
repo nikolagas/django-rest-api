0110e231db1c944312ae8ff5e7f65c117c41e11f
Nikola <nikolaos.todorov@gmail.com>
2023-06-26 11:07:10 +0200

    Added example view

--- profiles-rest-api/profiles_api/views.py
+++ profiles-rest-api/profiles_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            ''
+        ]
+
+        return Response({'message': 'Hi!', 'apiview': apiview})
